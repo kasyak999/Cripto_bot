0b0c10672e72bbc4fa76e389fc49012fae8c9da6
Максим <kasyak999@yandex.ru>
2025-07-09 22:39:03 +0300

    up

--- app/comand.py
+++ app/comand.py
@@ -154,6 +154,37 @@ def get_bot_start():
                 buy_coin(coin.name, BUY_USDT)
 
 
+def test_func():
+    """ Тестовая функция """
+    print('выставляем лимитный ордер')
+    # создать лимитный ордер
+    # order = session.place_order(
+    #     category="spot",          # спотовый рынок
+    #     symbol="BTCUSDT",         # торговая пара
+    #     side="Buy",               # "Buy" или "Sell"
+    #     orderType="Limit",       # лимитный ордер
+    #     qty=0.1,                # количество базовой валюты
+    #     price=100,              # цена лимитного ордера
+    # )
+
+
+    # # получить все ордеры
+    # orders = session.get_open_orders(
+    #     category="spot",         # или "linear" / "inverse"
+    # )
+    # # pprint(orders['result']['list'])
+    # for i in orders['result']['list']:
+    #     print(i['symbol'], i['orderId'])
+
+    # удалить ордер
+    cancel_result = session.cancel_order(
+        category="spot",              # "spot", "linear" или "inverse"
+        symbol="BTCUSDT",             # символ ордера
+        order_id="1991054721099762944"         # ID ордера, который нужно отменить
+    )
+    pprint(cancel_result)
+
+
 def buy_coin(symbol, price):
     """Купить монету"""
     ticker = validate_symbol(session, symbol)
